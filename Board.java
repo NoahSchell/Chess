2b18904c7e68091d7b4888f26c51459eaa0d91a3
NoahSchell <117382125+NoahSchell@users.noreply.github.com>
2022-11-07 11:22:22 -0600

    updates

--- Board.java
+++ Board.java
@@ -1,6 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class Board
 {
     public static void main(String[] args) {
-        System.out.println("Testing run!");
+        JFrame f = new JFrame("Test");
+        JButton button = new JButton("TestButton");
+        f.add(button);
+        f.setSize(200,200);
+        f.setVisible(true);
     }
 }
