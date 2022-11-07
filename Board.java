f5255d6f1d815cad64c968ed45f03f6d30d427cf
NoahSchell <117382125+NoahSchell@users.noreply.github.com>
2022-11-07 11:28:54 -0600

    Update

--- Board.java
+++ Board.java
@@ -2,13 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Board
+public class Board extends JFrame
 {
+    public Board() {
+        super("Chess");
+        Container win = getContentPane();
+
+    }
+
+
     public static void main(String[] args) {
-        JFrame f = new JFrame("Test");
-        JButton button = new JButton("TestButton");
-        f.add(button);
-        f.setSize(200,200);
-        f.setVisible(true);
+        new Board();
     }
 }
