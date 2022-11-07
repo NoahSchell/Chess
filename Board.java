99ccd9aa7aa213ad8b03be8933539482bd1e07c6
NoahSchell <117382125+NoahSchell@users.noreply.github.com>
2022-11-07 11:57:19 -0600

    update

--- Board.java
+++ Board.java
@@ -2,12 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Board extends JFrame
-{
+public class Board extends JFrame {
     public Board() {
         super("Chess");
         Container win = getContentPane();
+        win.setLayout(new BorderLayout());
 
+        JPanel p2 = new JPanel();
+        p2.setLayout(new FlowLayout());
+
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(8,8));
+
+        win.add(p1,BorderLayout.WEST);
+        win.add(p2,BorderLayout.CENTER);
+
+        setSize(500,500);
+        setVisible(true);
     }
 
 
@@ -16,4 +27,6 @@ public class Board extends JFrame
         Space [][] grid = new Space[8][8];
 
     }
+
+
 }
