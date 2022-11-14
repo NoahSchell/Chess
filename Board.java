94216cb5e85a76597801c4edcc2bb133ace463fd
NoahSchell <117382125+NoahSchell@users.noreply.github.com>
2022-11-14 13:32:10 -0600

    updates

--- Board.java
+++ Board.java
@@ -1,21 +1,23 @@
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
+import java.util.*;
 import java.awt.*;
-import java.io.*;
+import java.awt.event.*;
 
 //  FALSE == BLACK PIECE, TRUE == WHITE PIECE
 //  For now, click the piece, then click the space. We can add drag stuff later. 
 
-public class Board extends JFrame {
+public class Board extends JFrame{
     static Piece[] game = Piece.game;
     public static void main(String[] args) {
         Piece.setUpBoard();
         new Board();
     }
-    
+
     static Container win;
     static Color color1 = Color.decode("#61492f");
     static Color color2 = Color.decode("#d4bca3");
-
+    static JPanel p1; 
     public Board() {
         super("Chess");
         try { UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); } catch (Exception ex) {}
@@ -25,7 +27,7 @@ public class Board extends JFrame {
         JPanel p2 = new JPanel();
         p2.setLayout(new FlowLayout());
 
-        JPanel p1 = new JPanel();
+        p1 = new JPanel();
         p1.setLayout(new GridLayout(8, 8)); 
 
         win.add(p2, BorderLayout.WEST);
@@ -56,4 +58,42 @@ public class Board extends JFrame {
         setSize(800, 800);
         setVisible(true);
     }
+
+    
+    public class MouseStuff extends MouseInputAdapter
+    {
+        private Component component; 
+
+        public void mousePressed(MouseEvent e)
+        {
+            component = e.getComponent();
+            highlightLegalMoves(e.getSource());
+        }
+
+        public void highlightLegalMoves(Object obj)
+        {
+            if (obj instanceof Piece)
+            {
+                Piece piece = (Piece)obj;
+                ArrayList<Integer> squares = piece.getLegalMoves();
+                for(int x=0; x<64; x++)
+                    if (squares.contains(x))
+                    {
+                        //JPanel panelToChange = p1.getComponent(x/8, x%8);
+                    }
+            }
+        }
+
+        public void paintComponent(Graphics g)
+        {
+            for(int x = 0; x<64; x++)
+            {
+                if (game[x] != null)
+                {
+                    
+                }
+            }
+        }
+    }
+
 }
